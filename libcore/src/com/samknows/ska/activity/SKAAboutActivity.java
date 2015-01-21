86bbd5e3a94cbd57e459cfba61eb110282e9981a
Pete Cole <pete@samknows.com>
2015-01-21 14:19:30 +0000

    Fixed the About activity, to use standard "back button" processing.

--- libcore/src/com/samknows/ska/activity/SKAAboutActivity.java
+++ libcore/src/com/samknows/ska/activity/SKAAboutActivity.java
@@ -51,22 +51,4 @@ public class SKAAboutActivity extends BaseLogoutActivity {
 		Util.initializeFonts(this);
 		Util.overrideFonts(this, findViewById(android.R.id.content));
 	}
-	
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		switch (item.getItemId()) {
-		case android.R.id.home:
-			// This ID represents the Home or Up button. In the case of this
-			// activity, the Up button is shown. Use NavUtils to allow users
-			// to navigate up one level in the application structure. For
-			// more details, see the Navigation pattern on Android Design:
-			//
-			// http://developer.android.com/design/patterns/navigation.html#up-vs-back
-			//
-			NavUtils.navigateUpTo(this,
-					new Intent(this, SKApplication.getAppInstance().getTheMainActivityClass()));
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
 }
